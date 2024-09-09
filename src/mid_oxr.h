54304ea8d76cc6eff6643a7844fae32616fbf738
rygo6 <rygo6@me.com>
2024-09-08 20:10:06 -0700

    bunch to do with action and interactions filled out

--- src/mid_oxr.h
+++ src/mid_oxr.h
@@ -39,20 +39,56 @@ typedef struct IUnknown IUnknown;
 
 //
 /// Mid OpenXR Types
-
-
-// ya we want to do this
 typedef struct XrInstanceContext {
-	XrInstance  instance;
-}XrInstanceContext;
+	XrInstance instance;
+} XrInstanceContext;
 
 //
-/// Mid OpenXR Implementation
-static uint32_t CalcDJB2(const char *str, int max) {
-	char c; int i = 0; uint32_t hash = 5381;
+/// Mid OpenXR Utility
+static uint32_t CalcDJB2(const char* str, int max) {
+	char     c;
+	int      i = 0;
+	uint32_t hash = 5381;
 	while ((c = *str++) && i++ < max)
 		hash = ((hash << 5) + hash) + c;
 	return hash;
+}
+
+#define CLAIM_HANDLE(_type)               \
+	if (_type##Count >= _type##Capacity)    \
+		return XR_ERROR_LIMIT_REACHED;        \
+	const int handle = _type##Count++;      \
+	_type*    p##_type = &_type##s[handle]; \
+	*p##_type = (_type)
+
+//
+/// Mid Device Input Funcs
+
+static int OculusLeftClick(float* pValue) {
+	*pValue = 0;
+	return 0;
+}
+
+static int OculusRightClick(float* pValue) {
+	*pValue = 0;
+	return 0;
+}
+
+//
+/// Mid OpenXR Implementation
+
+typedef struct Path {
+	char string[XR_MAX_PATH_LENGTH];
+	void* chained;
+} Path;
+#define PathCapacity 128
+static int  PathCount = 0;
+static Path Paths[PathCapacity];
+static int  PathHashes[PathCapacity];
+
+static int GetPathHash(const Path* pPath) {
+	const int handle = (pPath - Paths) / sizeof(Path);
+	return PathHashes[handle];
 }
 
 XRAPI_ATTR XrResult XRAPI_CALL xrPathToString(
@@ -61,50 +97,210 @@ XRAPI_ATTR XrResult XRAPI_CALL xrPathToString(
 	uint32_t   bufferCapacityInput,
 	uint32_t*  bufferCountOutput,
 	char*      buffer) {
-	switch ((uint32_t)path) {
-#define CASE(_name, _djb2)                                                                          \
-	case _djb2:                                                                                       \
-		*bufferCountOutput = sizeof(_name) < bufferCapacityInput ? sizeof(_name) : bufferCapacityInput; \
-		memcpy(buffer, &_name, *bufferCountOutput);                                                     \
-		return XR_SUCCESS;
-
-		CASE("/user/hand/left", 2556700407)
-		CASE("/user/hand/right", 2773994890)
-		CASE("/user/hand/left/input/select/click", 335458810)
-		CASE("/user/hand/right/input/select/click", 1218722221)
-#undef CASE
-		default:
-			return XR_ERROR_FUNCTION_UNSUPPORTED;
-	}
+	Path* pPath = (Path*)path;
+	strncpy(buffer, (const char*)&pPath->string, bufferCapacityInput < XR_MAX_PATH_LENGTH ? bufferCapacityInput : XR_MAX_PATH_LENGTH);
+	*bufferCountOutput = strlen(buffer);
+	return XR_SUCCESS;
 }
-
 XRAPI_ATTR XrResult XRAPI_CALL xrStringToPath(
 	XrInstance  instance,
 	const char* pathString,
-	XrPath*   path) {
-	printf("xrStringToPath\n");
-	const uint32_t djb2 = CalcDJB2(pathString, XR_MAX_PATH_LENGTH);
-	*path = (XrPath)djb2;
-	return *path != 0 ? XR_SUCCESS : XR_ERROR_PATH_INVALID;
-}
-
-static int xrActionSetCount = 1;
-
-XRAPI_ATTR XrResult XRAPI_CALL xrCreateActionSet(
-	XrInstance                   instance,
-	const XrActionSetCreateInfo* createInfo,
-	XrActionSet*                 actionSet) {
-	*actionSet = (XrActionSet)(uint64_t)xrActionSetCount++;
+	XrPath*     path) {
+	const int hash = CalcDJB2(pathString, XR_MAX_PATH_LENGTH);
+	for (int i = 0; PathHashes[i] != 0 && i < PathCapacity; ++i) {
+		if (PathHashes[i] == hash) {
+			*path = (XrPath)&Paths[i];
+			return XR_SUCCESS;
+		}
+	}
+	CLAIM_HANDLE(Path){};
+	strncpy((char*)&pPath->string, pathString, XR_MAX_PATH_LENGTH);
+	pPath->string[XR_MAX_PATH_LENGTH - 1] = '\0';
+	PathHashes[handle] = CalcDJB2(pathString, XR_MAX_PATH_LENGTH);
+	*path = (XrPath)pPath;
 	return XR_SUCCESS;
 }
+
+typedef struct Binding {
+	const Path* pPath;
+	int (*func)(void*);
+} Binding;
+#define BindingCapacity 128
+static int     BindingCount = 0;
+static Binding Bindings[BindingCapacity];
+static int     BindingHashes[BindingCapacity];
+
+#define MIDXR_MAX_INTERACTION_PROFILE_BINDINGS 16
+typedef struct InteractionProfile {
+	const Path* pPath;
+	Binding bindings[MIDXR_MAX_INTERACTION_PROFILE_BINDINGS]
+} InteractionProfile;
+
+// this needs to register to interaction profiles
+static XrResult RegisterBinding(Path* pPath, int (*const func)(void*)) {
+	const int hash = GetPathHash(pPath);
+	for (int i = 0; i < BindingCount; ++i) {
+		if (PathHashes[i] == hash) {
+			fprintf(stderr, "Trying to register path hash twice! %s %d\n", pPath->string, hash);
+			return XR_ERROR_PATH_INVALID;
+		}
+	}
+	CLAIM_HANDLE(Binding){
+		.pPath = pPath,
+		.func = func,
+	};
+	BindingHashes[handle] = hash;
+	pPath->chained = pBinding; // this should chain somehow
+	return XR_SUCCESS;
+}
+static void InitStandardBindings(){
+	if (BindingCount != 0)
+		return;
+
+	{
+		const char* interactionProfile = "/interaction_profiles/oculus/touch_controller";
+		Path* leftPath;
+		xrStringToPath(NULL, "/user/hand/left/input/select/click", (XrPath*)&leftPath);
+		RegisterBinding(leftPath, (int (*)(void*))OculusLeftClick);
+		Path* rightPath;
+		xrStringToPath(NULL, "/user/hand/right/input/select/click", (XrPath*)&rightPath);
+		RegisterBinding(rightPath, (int (*)(void*))OculusRightClick);
+	}
+}
+
+#define MIDXR_MAX_SUBACTION_PATHS 2
+typedef struct Action {
+	char         actionName[XR_MAX_ACTION_NAME_SIZE];
+	XrActionType actionType;
+	uint32_t     countSubactionPaths;
+	const Path*  pSubactionPaths[MIDXR_MAX_SUBACTION_PATHS];
+	char         localizedActionName[XR_MAX_LOCALIZED_ACTION_NAME_SIZE];
+	//this only supports one suggested binding atm
+	//	const Path* bindings[MIDXR_MAX_SUBACTION_PATH_COUNT];
+	Binding* pBindings[MIDXR_MAX_SUBACTION_PATHS];
+} Action;
+#define ActionCapacity 64
+static int ActionCount = 1;
+Action     Actions[ActionCapacity];
+
+#define MIDXR_MAX_ACTION_SET_ACTION_COUNT 32
+typedef struct ActionSet {
+	char          actionSetName[XR_MAX_ACTION_SET_NAME_SIZE];
+	char          localizedActionSetName[XR_MAX_LOCALIZED_ACTION_SET_NAME_SIZE];
+	uint32_t      priority;
+	uint32_t      actionCount;
+	const Action* pActions[MIDXR_MAX_ACTION_SET_ACTION_COUNT];
+} ActionSet;
+#define ActionSetCapacity 32
+static int ActionSetCount = 0;
+ActionSet  ActionSets[ActionSetCapacity];
 
 XRAPI_ATTR XrResult XRAPI_CALL xrCreateAction(
 	XrActionSet               actionSet,
 	const XrActionCreateInfo* createInfo,
 	XrAction*                 action) {
-
+	printf("Creating Action %s with %d subactions.\n", createInfo->actionName, createInfo->countSubactionPaths);
+	CLAIM_HANDLE(Action){
+		.actionType = createInfo->actionType,
+		.countSubactionPaths = createInfo->countSubactionPaths,
+	};
+	if (createInfo->countSubactionPaths > MIDXR_MAX_SUBACTION_PATHS){
+		printf("countSubactionPaths %d greater than MIDXR_MAX_SUBACTION_PATHS %d\n", createInfo->countSubactionPaths, MIDXR_MAX_SUBACTION_PATHS);
+		return XR_ERROR_PATH_COUNT_EXCEEDED;
+	}
+	strncpy((char*)&pAction->actionName, (const char*)&createInfo->actionName, XR_MAX_ACTION_SET_NAME_SIZE);
+	strncpy((char*)&pAction->localizedActionName, (const char*)&createInfo->localizedActionName, XR_MAX_LOCALIZED_ACTION_SET_NAME_SIZE);
+	memcpy(&pAction->pSubactionPaths, createInfo->subactionPaths, pAction->countSubactionPaths * sizeof(XrPath));
+	*action = (XrAction)pAction;
+	ActionSet* pActionSet = (ActionSet*)actionSet;
+	const int  actionHandle = pActionSet->actionCount;
+	pActionSet->pActions[actionHandle] = pAction;
 	return XR_SUCCESS;
 }
+
+XRAPI_ATTR XrResult XRAPI_CALL xrCreateActionSet(
+	XrInstance                   instance,
+	const XrActionSetCreateInfo* createInfo,
+	XrActionSet*                 actionSet) {
+	CLAIM_HANDLE(ActionSet){
+		.priority = createInfo->priority};
+	strncpy((char*)&pActionSet->actionSetName, (const char*)&createInfo->actionSetName, XR_MAX_ACTION_SET_NAME_SIZE);
+	strncpy((char*)&pActionSet->localizedActionSetName, (const char*)&createInfo->localizedActionSetName, XR_MAX_LOCALIZED_ACTION_SET_NAME_SIZE);
+	*actionSet = (XrActionSet)pActionSet;
+	return XR_SUCCESS;
+}
+
+static int CompareSubPath(const char* pSubPath, const char* pPath) {
+	while (*pSubPath != '\0') {
+		if (pSubPath != pPath )
+			return 1;
+		pSubPath++;
+		pPath++;
+	}
+	return 0;
+}
+
+XRAPI_ATTR XrResult XRAPI_CALL xrSuggestInteractionProfileBindings(
+	XrInstance                                  instance,
+	const XrInteractionProfileSuggestedBinding* suggestedBindings) {
+	const Path* pInteractionProfilePath = (Path*)suggestedBindings->interactionProfile;
+	printf("Binding: %s\n", pInteractionProfilePath->string);
+	const XrActionSuggestedBinding* pSuggestedBindings = suggestedBindings->suggestedBindings;
+	for (int i = 0; i < suggestedBindings->countSuggestedBindings; ++i) {
+		Action*     pActionToBind = (Action*)pSuggestedBindings[i].action;
+		const Path* pBindingPath = (Path*)pSuggestedBindings[i].binding;
+		printf("Action: %s BindingPath: %s\n", pActionToBind->actionName, pBindingPath->string);
+		if (pActionToBind->countSubactionPaths == 0) {
+			printf("No Subactions. Bound to zero.\n");
+			pActionToBind->pBindings[0] = pBindingPath->chained;
+			continue;
+		}
+		for (int j = 0; j < pActionToBind->countSubactionPaths; ++j) {
+			const Path* pSubPath = (Path*)pActionToBind->pSubactionPaths[j];
+			if (CompareSubPath(pSubPath->string, pBindingPath->string)) continue;
+			printf("Bound to %d %s\n", j, pSubPath->string);
+			pActionToBind->pBindings[j] = pBindingPath->chained;
+		}
+	}
+	return XR_SUCCESS;
+}
+
+
+XRAPI_ATTR XrResult XRAPI_CALL xrGetActionStateFloat(
+	XrSession                   session,
+	const XrActionStateGetInfo* getInfo,
+	XrActionStateFloat*         state) {
+	const Action* pAction = (Action*)getInfo->action;
+	if (pAction->pBindings[0] == NULL) {
+		fprintf(stderr, "%s not bound l\n", pAction->actionName);
+		return XR_EVENT_UNAVAILABLE;
+	}
+	const Path* subactionPath = (Path*)getInfo->subactionPath;
+	if (subactionPath == NULL) {
+		float result;
+		pAction->pBindings[0]->func(&result);
+		state->currentState = result;
+		state->changedSinceLastSync = true;
+		state->lastChangeTime = 0;
+		state->isActive = true;
+		return XR_SUCCESS;
+	}
+	for (int i = 0; i < pAction->countSubactionPaths; ++i) {
+		const Path* pSubPath = (Path*)pAction->pSubactionPaths[i];
+		if (subactionPath == pSubPath) {
+			float result;
+			pAction->pBindings[i]->func(&result);
+			state->currentState = result;
+			state->changedSinceLastSync = true;
+			state->lastChangeTime = 0;
+			state->isActive = true;
+			return XR_SUCCESS;
+		}
+	}
+	fprintf(stderr, "%s subaction on %s not bound l\n", subactionPath->string,  pAction->actionName);
+	return XR_EVENT_UNAVAILABLE;
+}
+
 
 static int xrSpaceCount = 1;
 
@@ -225,18 +421,23 @@ XRAPI_ATTR XrResult XRAPI_CALL xrEnumerateApiLayerProperties(
 	return XR_SUCCESS;
 }
 
+// modulo to ensure jump table is generated
+// https://godbolt.org/z/rGz6TbsG1
+#define PROC_ADDR_MODULO 63
 XRAPI_ATTR XrResult XRAPI_CALL xrGetInstanceProcAddr(
 	XrInstance          instance,
 	const char*         name,
 	PFN_xrVoidFunction* function) {
+
+	InitStandardBindings();
+
 	printf("xrGetInstanceProcAddr %s\n", name);
-	const uint32_t djb2 = CalcDJB2(name, XR_MAX_RESULT_STRING_SIZE);
-	switch (djb2) {
 #define CASE(_name, _djb2)                 \
-	case _djb2:                              \
+	case _djb2 % PROC_ADDR_MODULO:           \
 		*function = (PFN_xrVoidFunction)_name; \
 		return XR_SUCCESS;
-
+	const uint32_t djb2 = CalcDJB2(name, XR_MAX_RESULT_STRING_SIZE) % PROC_ADDR_MODULO;
+	switch (djb2) {
 		CASE(xrGetInstanceProcAddr, 2334007635)
 		CASE(xrEnumerateApiLayerProperties, 1963032185)
 		CASE(xrEnumerateInstanceExtensionProperties, 1017617012)
@@ -245,15 +446,17 @@ XRAPI_ATTR XrResult XRAPI_CALL xrGetInstanceProcAddr(
 		CASE(xrEnumerateViewConfigurationViews, 4194621334)
 		CASE(xrGetOpenGLGraphicsRequirementsKHR, 2661449710)
 		CASE(xrCreateSession, 612805351)
+		CASE(xrGetActionStateFloat, 1753855652)
 		CASE(xrCreateReferenceSpace, 1138416702)
 		CASE(xrCreateActionSet, 2376838285)
 		CASE(xrCreateAction, 613291425)
 		CASE(xrStringToPath, 1416299542)
 		CASE(xrPathToString, 3598878870)
-#undef CASE
+		CASE(xrSuggestInteractionProfileBindings, 3618139248)
 		default:
 			return XR_ERROR_FUNCTION_UNSUPPORTED;
 	}
+#undef CASE
 }
 
 XRAPI_ATTR XrResult EXPORT XRAPI_CALL xrNegotiateLoaderRuntimeInterface(
